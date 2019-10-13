88c08878b8e104c65aa55d6e581505e79e26b16e
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-10-13 08:16:34 +0300

    Организация структуры проекта

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const nunjucks = require('nunjucks')
 const express = require('express');
+var path = require('path');
 const app = express();
 
 nunjucks.configure({
@@ -10,17 +11,17 @@ nunjucks.configure({
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
-  res.render('index.html')
+  res.render(path.join(__dirname, '/index.html'))
 });
 
 app.get('/form', function (req, res) {
-  res.render('form-response.html', {
+  res.render(path.join(__dirname, '/form-response.html'), {
     getRequest: req.query
   })
 })
 
 app.post('/form', function (req, res) {
-  res.render('form-response.html', {
+  res.render(path.join(__dirname, '/form-response.html'), {
     postRequest: req.body
   })
 })
